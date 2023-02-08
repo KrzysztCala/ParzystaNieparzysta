de43bf7fc0c60c1a06beace80009c84658d2eda9
KrzysztofCała <k.cala@parsifal.com.pl>
2023-02-08 17:18:11 +0100

    Pierwszy Commit

--- ParzystaNieparzysta/Program.cs
+++ ParzystaNieparzysta/Program.cs
@@ -10,9 +10,45 @@ namespace ParzystaNieparzysta
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("PROGRAM OKREŚLANIA PARZYSTOŚCI/NIEPARZYSTOŚCI LICZBY\n");
+            Console.WriteLine("Naciśnij w dowolnym momencie Ctrl-C, aby zakończyć.\n\n");
+            do
+            {
+                Console.Write("\nWpisz liczbę: ");
+                try
+                {
+                    int number1 = GetIntInput();
+                    if(number1 % 2 == 0)
+                    {
+                        Console.WriteLine("Wprowadzona liczba jest parzysta.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wprowadzona liczba jest nieparzysta.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Spróbuj ponownie!");
+                    continue;  
+                }
+            } while (true);
 
 
         }
+
+        private static int GetIntInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa.");
+            }
+            return number;
+        }
+
     }
+
+
+
 }
